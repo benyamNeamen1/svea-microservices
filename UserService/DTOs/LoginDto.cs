ff33dfc578fea48a78126b64ae8081875b9c5214
Benyam Neamen <benyamneamen@MacBook-Pro-som-tillhor-benyam.local>
2025-11-29 12:37:33 +0100

    companycodes

--- UserService/DTOs/LoginDto.cs
+++ UserService/DTOs/LoginDto.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel;
+
 namespace Svea.UserService.DTOs
 {
     public class LoginDto
     {
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
+         [DefaultValue("ecfa2b93-8487-4713-b340-4878d9b8dc50")]
         public Guid CompanyId { get; set; }
     }
 }
